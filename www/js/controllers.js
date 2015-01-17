9b79407b5805644ad299b2d2b46f69ba2f7f4804
Gerben Neven <gerbyzation@me.com>
2015-01-17 14:56:27 +0000

    camera works

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,8 @@
 angular.module('starter.controllers', ['ngCordova'])
 
+
 .controller('DashCtrl', function($scope) {})
+
 
 .controller('ChatsCtrl', function($scope, Chats) {
   $scope.chats = Chats.all();
@@ -9,17 +11,21 @@ angular.module('starter.controllers', ['ngCordova'])
   };
 })
 
+
 .controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
   $scope.chat = Chats.get($stateParams.chatId);
 })
+
 
 .controller('FriendsCtrl', function ($scope, Friends) {
   $scope.friends = Friends.all();
 })
 
+
 .controller('FriendDetailCtrl', function ($scope, $stateParams, Friends) {
   $scope.friend = Friends.get($stateParams.friendId);
 })
+
 
 .controller('AccountCtrl', function($scope, Ratings) {
   $scope.settings = {
@@ -29,35 +35,33 @@ angular.module('starter.controllers', ['ngCordova'])
 
 })
 
+
+
+
+
+
 .controller('PictureCtrl', function ($scope, $cordovaCamera) {
-  $scope.data = "test";
-  $scope.imgURL = null;
-
-  var options = {
-    destinationType: Camera.DestinationType.NATIVE_URI,
-    sourceType: Camera.PictureSourceType.CAMERA,
-    saveToPhotoAlbum: true
-  };
-
-  // according to documentation deviceready eventlistener should be added
   $scope.getPicture = function () {
-    console.log('executing getPicture()');
+    var options = {
+      quality: 100,
+      destinationType: Camera.DestinationType.FILE_URI,
+      sourceType: Camera.PictureSourceType.CAMERA,
+      saveToPhotoAlbum: true
+    };
 
     $cordovaCamera.getPicture(options).then(function (imageURI) {
       console.log("succes callback");
-      console.log(imageURI);
       $scope.imgURL = imageURI;
     }, function (err ) {
-      console.log('error callback');
       console.error(err);
     });
-
-    $cordovaCamera.cleanup().then( function () {
-      // what's cleanup?
-    });
   };
-
 })
+
+
+
+
+
 
 .controller('sqlCtrl', function ($scope, $rootScope, Ratings) {
 
