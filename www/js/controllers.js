ae115782dba4a38e685a499bf7f9e8a4415a494a
Gerben Neven <gerbyzation@me.com>
2015-01-17 02:49:56 +0000

    abstracting sql-interaction to sevice, not working yet

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -52,81 +52,14 @@ angular.module('starter.controllers', ['ngCordova'])
 
 })
 
-.controller('sqlCtrl', function ($scope, $cordovaSQLite) {
+.controller('sqlCtrl', function ($scope, Ratings) {
 
   $scope.data = "Data";
-  $scope.ratings = null;
 
-  var db;
-
-  // open (and if necessary initialize db)
-  document.addEventListener("deviceready", function () {
-    db = window.openDatabase('rating', '1.0', 'Rating DB', 100000000);
-
-    var query = "CREATE TABLE IF NOT EXISTS ratings (ID integer unique primary key, item_name text, item_URI text, item_loc text, item_criteria text, item_rating integer);";
-    $cordovaSQLite.execute(db, query, []).then(function (res) {
-      // do nothing
-    }, function (err) {
-      console.error(err);
-    });
-
-    $cordovaSQLite.execute(db, 'SELECT * FROM ratings;', []).then(function (res) {
-      console.log(res.rows.length);
-      var items = [];
-      for (var i = 0; i < res.rows.length; i++ ) {
-        items.push(res.rows.item(i));
-      }
-      $scope.ratings =  items;
-    }, function (err) {
-      console.error(err);
-    });
-
-  }, false);
-
-  $scope.insert = function (name, URI, loc, criteria, rating) {
-
-    var query = "INSERT INTO ratings (item_name, item_URI, item_loc, item_criteria, item_rating) VALUES (?, ?, ?, ?, ?);";
-    $cordovaSQLite.execute(db, query, [name, URI, loc, criteria, rating]).then(function (res) {
-      var items = [];
-      for (var i = 0; i < res.rows.length; i++ ) {
-        items.push(res.rows.item(i));
-      }
-      $scope.ratings =  items;
-      console.log(res);
-    }, function (err) {
-      console.error(err);
-    });
-  };
-
-  $scope.nukeAll = function () {
-    var query = "DELETE FROM ratings WHERE ID=*;";
-
-    $cordovaSQLite.execute(db, query, []).then(function (res) {
-      console.log("Deleted all DB entries");
-      var items = [];
-      for (var i = 0; i < res.rows.length; i++) {
-        items.push(res.rows.item(i));
-      }
-      $scope.ratings =  items;
-      console.log(items);
-    }, function (err) {
-      console.error(err);
-    });
-  };
-
-  $scope.selectAll = function () {
-    var query = "SELECT * FROM ratings;";
-    $cordovaSQLite.execute(db, query, []).then(function (res) { 
-      console.log();
-      var items = [];
-      for (var i = 0; i < res.rows.length; i++ ) {
-        items.push(res.rows.item(i));
-      }
-      $scope.ratings =  items;
-    }, function (err) {
-      console.error(err);
-    });
-  };
+  $scope.ratings = Ratings.selectAll();
+  $scope.insert = Ratings.insert();
+  $scope.nukeAll = Ratings.nukeAll;
+  
 
 
 });
